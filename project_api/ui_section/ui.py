295f2179aeb507c65409b58e3a7403b70b6e1193
Julian Andres Corral Gomez <114829588+JulianCorralG@users.noreply.github.com>
2023-09-05 09:38:41 -0500

    Update ui.py
    little modification in variable crop_to_check, I add .capitalize to it

--- project_api/ui_section/ui.py
+++ project_api/ui_section/ui.py
@@ -1,7 +1,7 @@
 def get_parameters_to_query_in_database():
     departament_to_check = input("Department: ").upper()
     municipality_to_check = input("Municipality: ").upper()
-    crop_to_check = input("Crop: ")
+    crop_to_check = input("Crop: ").capitalize()
     limit_of_query = int(input("Limit of query: "))
     dictionary_with_keyword_arguments = {'departament_to_check': departament_to_check, 'municipality_to_check': municipality_to_check, 'crop_to_check': crop_to_check, 'limit_of_query': limit_of_query}
     return dictionary_with_keyword_arguments
