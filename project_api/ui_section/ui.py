636202e5980878354f47598910fabc299416c359
Julian Andres Corral Gomez <114829588+JulianCorralG@users.noreply.github.com>
2023-09-06 10:31:59 -0500

    Update ui.py
    little modification in ui section

--- project_api/ui_section/ui.py
+++ project_api/ui_section/ui.py
@@ -3,7 +3,10 @@ def get_parameters_to_query_in_database():
     municipality_to_check = input("Municipality: ").upper()
     crop_to_check = input("Crop: ").capitalize()
     limit_of_query = int(input("Limit of query: "))
-    dictionary_with_keyword_arguments = {'departament_to_check': departament_to_check, 'municipality_to_check': municipality_to_check, 'crop_to_check': crop_to_check, 'limit_of_query': limit_of_query}
+    dictionary_with_keyword_arguments = {'departament_to_check': departament_to_check,
+                                         'municipality_to_check': municipality_to_check,
+                                         'crop_to_check': crop_to_check,
+                                         'limit_of_query': limit_of_query}
     return dictionary_with_keyword_arguments
 
 def show_dataframe_from(dataframe_renamed):
